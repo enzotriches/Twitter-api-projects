35009e1c64e42a571f202d6640cfd42497e690d2
Enzo Triches <enzotriches20@gmail.com>
2019-09-11 01:32:19 -0300

    Adicionando Trends

--- twitter-api-00-with-php/index.php
+++ twitter-api-00-with-php/index.php
@@ -3,16 +3,45 @@
 require "vendor/autoload.php";
 
 use Abraham\TwitterOAuth\TwitterOAuth;
-//CHAVES DE ACESSO
-$token_de_acesso = "TOKEN_DE_ACESSO";
-$token_secreto_de_acesso = "TOKEN_SECRETO_DE_ACESSO";
+//CHAVES DE ACESSO - altere as chaves para as suas, pois quando você as utilizar já estão inativas
+$token_de_acesso = "3004691099-P2wNJJKPQMBXKRMrfqKIL9cgvsmBbhVqUx91JQI";
+$token_secreto_de_acesso = "4XIVG2N181rzJcCAjhh9S9lHkq4V3MRuNn52VgbpBAYzx";
 //INICIALIZAÇÃO DO TWITTEROAUTH
-$connection = new TwitterOAuth("CHAVE_PUBLICA_DE_CONSUMIDOR", "CHAVE_PRIVADA_DE_CONSUMIDOR", $token_de_acesso, $token_secreto_de_acesso);
+$connection = new TwitterOAuth("x3wCUrbwhZkZyWL4RzKDUOZGE", "nKoS6SH4ugwhh4bI1ZZP7IIaQwXbihbT0kcFjzTYRs7Ssb5YX6", $token_de_acesso, $token_secreto_de_acesso);
 //VERIFICA AS CREDENCIAIS
 $content = $connection->get("account/verify_credentials");
 //PEGA OS ULTIMOS 25 TWEETS
 $tweets = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_replies" => true]);
-?>
+//CÓDIGOS WOEID DISPONÍVEIS DA REGIÃO DO BRASIL
+$locais =  [
+  23424768,
+  455819,
+  455820,
+  455821,
+  455822,
+  455823,
+  455824,
+  455825,
+  455826,
+  455827,
+  455828,
+  455830,
+  455831,
+  455833,
+  455834,
+  455867
+];
+//USANDO MÉTODO PRECÁRIO PARA VERIFICAR SE ESTAMOS RECEBENDO UMA REQUEST DO FORMULÁRIO DE INSERÇÃO DO TWEET
+    if(isset($_POST['btn-tt'])){
+      //PEGA O TWEET
+      $tweet = $_POST['tweet'];
+      //POSTA O TWEET NA SUA CONTA
+      $status = $connection->post("statuses/update", ["status" => $tweet]);
+      //ATUALIZA O SEU FEED PEGANDO NOVAMENTE OS DADOS DA SUA TIMELINE
+      $tweets = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_replies" => true]);
+     }
+
+  ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,7 +57,7 @@ $tweets = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_re
   
 </head>
 <body class="bg-light">
-  <!--   Nav -->
+  <!--   H E A D E R   -->
   <nav class="nav navbar bg-primary border-bottom p-1">
     <div class="container-fluid text-center justify-content-center">
       <a href="" class="navbar-brand">
@@ -37,58 +66,105 @@ $tweets = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_re
       <span class="h3 text-light">Teste de Api do Twitter!</span>
     </div>
   </nav>
-  <?php
-     if(isset($_POST['btn-tt'])){
-       $tweet = $_POST['tweet'];
-       $status = $connection->post("statuses/update", ["status" => $tweet]);
-       
-        $tweets = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_replies" => true]);
-     }
-  ?>
-  <div class="container border shadow mt-1 mb-1 shadow p-1 ">
+  <!-- /  H E A D E R   -->
+  <!-- P O S T   -->
+  <div class="container border shadow rounded mt-1 mb-1 shadow p-1 ">
     <form action="index.php" method="POST">
       <div class="row">
-        <div class="col-12">
-          <input type="text" name="tweet" placeholder="O que você está pensando?..." class="form-control border border-primary p-3"/>
+        <div class="col-10">
+          <center>
+          <input type="text" name="tweet" placeholder="O que você está pensando?..." class="form-control border border-primary p-4"/>
+          </center>
         </div>
-        <div class="col-12 justify-content-end  p-1">
-          <input name="btn-tt" type="submit" class=" ml-2 btn btn-lg btn-primary btn-rounded rounded border text-light p-1 h2" value="Twittar!">
+        <div class="col-2 justify-content-end ">
+          <input name="btn-tt" type="submit" class="btn btn-lg btn-primary  rounded border text-light p-2 justify-content-end" value="Twittar!">
         </div>
       </div>
     </form>
   </div>
-  <!--  /Nav -->
-  <!--  Errors  -->
+  <!-- /P O S T   -->
+
+  <?php  //SE A ULTIMA REQUISIÇÃO HTTP DEU CERTO: ?>
   <?php  if($connection->getLastHttpCode() == 200){ ?>
+  <!-- N A V -->
   <div class="container text-center mt-1  justify-content-center">
-   
-    <div class="alert alert-success alert-dismissible fade show"   role="alert">
-      Ultima requisição deu certo!
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
- 
-   
-   </div>
-  <!--  Tweets  -->
+     <!-- N A V    I T E N S -->
+    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+      <li class="nav-item">
+        <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">
+        Tweets
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="pills-trends-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">
+        Trends
+        </a>
+      </li>
+      
+    </ul>
+    <!-- /N A V    I T E N S -->
+  </div>
+  <!-- N A V -->
+  <!--  T O D O   -->
   <div class="container border-primary  bg-light  shadow p-2 rounded mb-3 ">
-      <?php foreach($tweets as $data): ?>
-    
-         <div class="container border border-primary shadow bg-white rounded mb-2 p-2">
+    <!--  C O N T E U D O S   -->
+    <div class="tab-content" id="pills-tabContent">
+      <!--  T I M E    L I N E   -->
+      <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+        <?php //LISTA OS TWEETS ?>
+        <?php foreach($tweets as $data): ?>
+      
+          <div class="container border border-primary shadow bg-white rounded mb-2 p-2">
             <div class="row ">
               <div class="col-12">
-               <?php echo "O usuário <b>".$data->user->name."</b> tweetou:";?>
+                <?php echo "O usuário <b>".$data->user->name."</b> tweetou:";?>
               </div>
               <div class="col-12 p-1">
                 <div class="container">
-                 <?php echo $data->text; ?>
+                  <?php echo $data->text; ?>
                 </div>
               </div>
             </div>
-         </div>
+          </div>
+
+        <?php endforeach?> 
     
-       <?php endforeach?> 
+      </div>
+       <!-- / T I M E    L I N E   -->
+      <!-- T R E N D S  -->
+      <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-trends-tab">
+        <?php 
+              //LISTA OS TRENDS QUE CADA REGIÃO ESPECIFICADA PELO CÓDIGO WOEID
+              foreach($locais as $data):
+                //transformando std em array
+                $obj_trends = (array)$connection->get("trends/place",["id" => $data]);
+                $trends = json_decode(json_encode($obj_trends), True);
+                  
+                  if(isset($trends)){
+                    if(count($trends)>0){
+
+                  $local = $trends[0]['locations'][0]['name'];
+                  $trend_local = $trends[0]["trends"][0]["name"];
+                  $url_local = $trends[0]["trends"][0]["url"];
+
+        ?>
+               <div class="container border border-primary shadow bg-white rounded mb-2 p-2">
+                <div class="row ">
+                  <div class="col-12">
+                    <?php echo "Em <b>".$local."</b> a coisa mais falada é:";?>
+                  </div>
+                  <div class="col-12 p-1">
+                    <div class="container">
+                      <?php echo '<a class="text-dark" href="'.$url_local.'" target="_blank">'.$trend_local."</a>"; ?>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        <?php   }} endforeach ?>
+      </div>
+      <!-- /T R E N D S  -->
+    </div>
+    <!-- / C O N T E U D O S   -->
   <?php  }else{   ?>
      <div class="alert alert-primary alert-dismissible fade show" role="alert">
       Ultima requisição não deu certo!
@@ -97,8 +173,7 @@ $tweets = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_re
       </button>
     </div>
   <?php   }  ?>
-  <!--  /Errors  -->
+  <!--  /T O D O   -->
   </div>
-  
 </body>
 </html>
